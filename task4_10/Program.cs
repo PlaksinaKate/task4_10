74a15aeb3b7751c2dbebae6b89daabf44d77b8c9
eplaksina <eplaksina@angels.ru>
2021-11-22 23:49:20 +0300

    Добавьте файлы проекта.

---
+++ task4_10/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using task4_10;
+
+namespace task4_10
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ChildProgrammer programmer1 = new ChildProgrammer("Иванов", 5, 5, 2);
+
+            Console.WriteLine("Фамилия программиста: " + programmer1.surname);
+            Console.WriteLine("Число программ, написанных программистом: " + programmer1.numberOfPrograms);
+            Console.WriteLine("Число языков: " + programmer1.languages);
+            Console.WriteLine("Число правильно работающих программ: " + programmer1.rightProgram);
+            Console.WriteLine("Число программ умноженное на число языков и на число правильных программ и разделенное на число всех программ: " + programmer1.CountProgram());
+
+        }
+    }
+
+}
+
---
+++ task4_10/Programmer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4_10
+{
+    internal class Programmer
+    {
+        public string surname;
+        public int numberOfPrograms;
+        public int languages;
+
+        public Programmer(string surname, int numberOfPrograms, int languages)
+        {
+            this.surname = surname;
+            this.numberOfPrograms = numberOfPrograms;
+            this.languages = languages;
+        }
+
+        public string Surname
+        {
+            get {  return surname; }
+            set {  this.surname = value;}
+        }
+
+        public int NumberOfPrograms
+        {
+            get {  return this.numberOfPrograms;}
+            set {  this.numberOfPrograms = value;}
+        }
+
+        public int Languages
+        {
+            get {  return languages; }
+            set {  this.languages = value; }
+        }
+
+        public virtual int CountProgram()
+        {
+            return numberOfPrograms * languages;
+        }
+
+
+    }
+
+    internal class ChildProgrammer : Programmer
+    {
+        public int rightProgram;
+        public ChildProgrammer(string surname, int numberOfPrograms, int languages, int rightProgram) : base(surname, numberOfPrograms, languages)
+        { 
+            this.numberOfPrograms = numberOfPrograms;
+            this.rightProgram = rightProgram;
+            this.languages = languages;
+            this.surname = surname;
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { this.surname = value; }
+        }
+
+        public int NumberOfPrograms
+        {
+            get { return this.numberOfPrograms; }
+            set { this.numberOfPrograms = value; }
+        }
+
+        public int Languages
+        {
+            get { return languages; }
+            set { this.languages = value; }
+        }
+
+
+        public override int CountProgram()
+        {
+            int countProgram = base.CountProgram();
+            return countProgram * rightProgram / numberOfPrograms;
+        }
+
+
+    }
+}
